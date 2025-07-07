d50f2a0a6d2be0882bbc8897efa97e22f85a3c7c
JosephMzp <202111354@urp.edu.pe>
2025-07-07 06:17:44 -0500

    api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import "dotenv/config";
 import app from './app.js';
 import { connectDB } from "./db.js"
 
+app.get('/', (req, res) => {
+  res.send('✅ API Rafa Gym está online');
+});
 
 connectDB();
 app.listen(3000)
