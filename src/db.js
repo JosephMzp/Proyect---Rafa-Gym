63e0fa234ae6d3c5dc385b2c7938f63106865f57
JosephMzp <202111354@urp.edu.pe>
2025-07-07 05:42:47 -0500

    variables de entorno

--- src/db.js
+++ src/db.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-const password = encodeURIComponent('Pzmj8292.');
-const MONGODB_URI = `mongodb+srv://josephzp:${password}@rafagym.bq1dywn.mongodb.net/gym`
-export const connectDB = async() => {
-   try{
-      await mongoose.connect(MONGODB_URI)
-      console.log(">>>DB is connected")
-   }
-   catch(error)
-   {
-    console.log(error)
-   }
-}
+const uri = process.env.MONGODB_URI;
+if (!uri) throw new Error("Falta la variable MONGODB_URI");
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log(">>> DB conectada");
+  } catch (error) {
+    console.error("Error conectando a DB:", error);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import app from './app.js';
 import { connectDB } from "./db.js"
+
 
 connectDB();
 app.listen(3000)
